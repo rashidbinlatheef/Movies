077563699565f9d769bb2ce987710804ee27e0c9
Muhammed Rashid <rashidbinlatheef@gmail.com>
2024-09-02 13:16:53 +0530

    [CHORE] API Key added view Secret config

--- Movies/Movies/Constants.swift
+++ Movies/Movies/Constants.swift
@@ -7,4 +7,11 @@
 
 import Foundation
 
-let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
+struct Constants {
+    static let apiKey: String = {
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
+            fatalError("API_KEY is missing from Info.plist")
+        }
+        return key
+    }()
+}
